1b77775d2ac14b7b57339e805654396a41a34328
Midul-Sharma31 <mridulsharma3130@gmail.com>
2025-10-25 17:23:05 +0530

    minor bug fix

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -7,7 +7,7 @@ class ApiError extends Error {
         stack=""
     ){
         super(message)
-        this,statusCode = statusCode
+        this.statusCode = statusCode
         this.data = null,
         this.message=message,
         this.success=false,
